35e8245c718097475c71c791560fc43d3ae8f395
Transient-Onlooker <Transient-Onlooker@users.noreply.github.com>
2026-04-05 02:37:25 +0900

    Bump version to 2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.openflight4and.app.android"
         minSdk = 33
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.5.11"
+        versionCode = 2600
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
